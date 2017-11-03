2a2b3d75921247c9f9da79548c237f697957c37c
mark-barrett <mark.barrett3@mydit.ie>
2017-11-03 12:32:07 +0200

    Changed path

--- server.js
+++ server.js
@@ -14,14 +14,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
 // Angular DIST output folder
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // API location
 app.use('/api', api);
 
 // Send all other requests to the Angular app
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build/index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 //Set Port
