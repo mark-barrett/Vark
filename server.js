8542b2fc3345edaa7658de55b5267c44af9897e8
mark-barrett <mark.barrett3@mydit.ie>
2017-11-03 12:35:40 +0200

    Changed output folder for Angular

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
 // Angular DIST output folder
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'src')));
 
 // API location
 app.use('/api', api);
